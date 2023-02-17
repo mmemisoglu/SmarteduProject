76b2559572110d4f801cf903075d07d121342396
Mustafa Memişoğlu <m.memisoglu04@gmail.com>
2023-02-17 15:59:46 +0300

    Lesson 01

--- app.mjs
+++ app.mjs
@@ -2,10 +2,20 @@ import express from "express";
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+//MIDDLEWARES
+app.use(express.static("public"));
+
+
+//ROUTER
 app.get("/", (req, res) => {
   res.status(200).send("INDEX SAYFASI");
 });
 
+
+//Sever listen
 const port = 3000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
