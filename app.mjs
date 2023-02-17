5b55f519f23c55d854233753a7c22a2af129a9cc
Mustafa Memişoğlu <m.memisoglu04@gmail.com>
2023-02-17 16:55:16 +0300

    Lesson 02

--- app.mjs
+++ app.mjs
@@ -8,12 +8,17 @@ app.set("view engine", "ejs");
 //MIDDLEWARES
 app.use(express.static("public"));
 
-
 //ROUTER
 app.get("/", (req, res) => {
-  res.status(200).send("INDEX SAYFASI");
+    res.status(200).render("index", {
+        page_name: "index"
+    });
 });
-
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+        page_name: "about"
+    });
+});
 
 //Sever listen
 const port = 3000;
