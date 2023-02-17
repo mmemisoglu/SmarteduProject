7cab4687e9dfb615b0fc30097a3326e828b6ff40
Mustafa Memişoğlu <m.memisoglu04@gmail.com>
2023-02-18 00:34:43 +0300

    Lesson 06 - list a course

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,15 +1,13 @@
 import Course from "../models/Course.js";
 
 export const createCourse = async (req, res) => {
-  
   try {
     const course = await Course.create(req.body);
     res.status(201).json({
       status: "success",
       course,
     });
-   } 
-  catch {
+  } catch (error) {
     res.status(400).json({
       status: "fail",
       error,
@@ -18,3 +16,18 @@ export const createCourse = async (req, res) => {
 };
 
 
+export const getAllCourse = async (req, res) => {
+  try {
+    const courses = await Course.find();
+
+    res.status(200).render('courses', {
+      courses,
+      page_name: 'courses',
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -5,6 +5,7 @@ import * as courseController from "../controllers/courseController.js";
 const router = express.Router();
 
 router.route('/').post(courseController.createCourse);
+router.route('/').get(courseController.getAllCourse);
 
 //EXPORT
 export default router;
