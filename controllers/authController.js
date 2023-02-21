080a82538f38a782696d164188b54f5b95036ade
Mustafa Memişoğlu <m.memisoglu04@gmail.com>
2023-02-21 20:29:14 +0300

    Lesson 13 - Dashboard

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,7 +29,7 @@ export const loginUser = async (req, res) => {
           if(same) {
             // USER SESSION
             req.session.userID = user._id;
-            res.status(200).redirect("/");
+            res.status(200).redirect("/users/dashboard");
           }
         }) 
       }
@@ -48,6 +48,14 @@ export const logoutUser = (req, res) => {
     res.redirect("/");
   })
 }
+
+export const getDashboardPage = async (req, res) => {
+  const user = await User.findOne({_id: req.session.userID})
+  res.status(200).render("dashboard", {
+    page_name: "dashboard",
+    user
+  });
+};
 
 
 
