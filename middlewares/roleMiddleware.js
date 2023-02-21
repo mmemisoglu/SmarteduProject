23cee018ab19729152b793157e89d7a96f59af05
Mustafa Memişoğlu <m.memisoglu04@gmail.com>
2023-02-21 23:00:04 +0300

    Lesson 15 - User Roles

---
+++ middlewares/roleMiddleware.js
@@ -0,0 +1,11 @@
+
+export const roleCheck = (roles) => {
+    return (req,res,next) => {
+        const userRole = req.body.role;
+        if(roles.includes(userRole)) {
+            next();
+        }else{
+            return res.status(401).send("YOU CANT DO IT");
+        }
+    }
+}
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,10 +1,10 @@
 //IMPORT
 import express from "express";
 import * as courseController from "../controllers/courseController.js";
-
+import * as roleMiddleware from "../middlewares/roleMiddleware.js";
 const router = express.Router();
 
-router.route('/').post(courseController.createCourse);
+router.route('/').post(roleMiddleware.roleCheck(["teacher","admin"]) ,courseController.createCourse);
 router.route('/').get(courseController.getAllCourse);
 router.route('/:slug').get(courseController.getCourse);
 
